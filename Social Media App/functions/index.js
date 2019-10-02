c2f8c925af2c210590ba904066a4195d3bdfefc9
Rui Han <hrcn1128@gmail.com>
2019-10-02 01:17:48 -0400

    user registration

--- Social Media App/functions/index.js
+++ Social Media App/functions/index.js
@@ -1,14 +1,27 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const app = require('express')();
 
 admin.initializeApp();
 
-const express = require('express');
-const app = express();
+const firebaseConfig = {
+    apiKey: "AIzaSyAW0PslpBbrSjN-8HnazZ_yUAX8BxCrYfQ",
+    authDomain: "social-media-app-c5b59.firebaseapp.com",
+    databaseURL: "https://social-media-app-c5b59.firebaseio.com",
+    projectId: "social-media-app-c5b59",
+    storageBucket: "social-media-app-c5b59.appspot.com",
+    messagingSenderId: "750572210139",
+    appId: "1:750572210139:web:a8731944985cbf62e509fe",
+    measurementId: "G-QCY0SXSRYW"
+  };
+
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig);
+
+const db = admin.firestore();
 
 app.get('/screams', (req, res) => {
-    admin
-    .firestore()
+    db
     .collection('screams')
     .orderBy('createdAt', 'desc')
     .get()
@@ -34,8 +47,7 @@ app.post('/scream', (req, res) => {
         createdAt: new Date().toISOString()
     };
 
-    admin
-    .firestore()
+    db
     .collection('screams')
     .add(newScream)
     .then(doc => {
@@ -47,13 +59,42 @@ app.post('/scream', (req, res) => {
     });
 });
 
-// exports.createScream = functions.https.onRequest((req, res) => {
-//     if(req.method !== 'POST') {
-//         return res.status(400).json({ error: 'Method not allowed' });
-//     }
-    
-    
-    
-// });
+// SIGN UP ROUTE
+app.post('/signup', (req, res) => {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle
+    };
+    firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+        .then(data => {
+            return res.status(201).json({ message: 'user ${data.user.uid} signed up successfully' });
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({ error: err.code });
+        })
+})
+
+// VALIDATE DATA
+db.doc('/users/${newUser.handle}').get()
+    .then(doc => {
+        if(doc.exists){
+            return res.status(400).json({ handle: 'this handle is already taken' });
+        } else {
+            firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password);
+        }
+    })
+    .then(data => {
+        return data.user.getIdToken();
+    })
+    .then(token => {
+        return res.status(201).json({ token });
+    })
+    .catch(err => {
+        console.error(err);
+        return res.status(500).json({ error: err.code });
+    })
 
 exports.api = functions.https.onRequest(app);
