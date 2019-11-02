ae1cfbf7c6dec086316bae24a84faf4de428c83a
Rui Han <hrcn1128@gmail.com>
2019-11-02 19:08:57 -0400

    add Hand & Face Route

---
+++ Fortune Teller/src/pages/Hand.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Hand extends Component {
+    render() {
+        return (
+            <div>
+                <h1>HAND IMAGE PREDICTION</h1>
+            </div>
+        )
+    }
+}
+
+export default Hand;
