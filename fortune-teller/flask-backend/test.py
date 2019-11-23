42a79446e649f4d2f04b97a6da992a2e86622ce8
Rui Han <hrcn1128@gmail.com>
2019-11-23 16:26:01 -0500

    move fortuen teller to another repo

--- fortune-teller/flask-backend/test.py
+++
@@ -1,6 +0,0 @@
-def hello():
-    return "Hi there!"
-
-def generate_text(userquestion):
-    answer = userquestion + "111"
-    return answer
--- fortune-teller/server/config.js
+++
@@ -1,8 +0,0 @@
-const env = {
-    host: 'localhost',
-    user: 'root',
-    password: 'info6210popeyes',
-    database: 'test'
-};
-
-module.exports = env;
